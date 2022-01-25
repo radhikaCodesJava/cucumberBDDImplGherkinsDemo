3451bdacc88cc712cf279940d9516182b6dc1592
radhika inguva <radhika.inguva@gmail.com>
2022-01-24 16:31:34 -0800

    adding test runner class

---
+++ src/test/java/testRunner/testRunner.java
@@ -0,0 +1,12 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+//import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(features="C://Users//balum//eclipse-workspace//CucumberDemoBDD//Features//LMSAPI.feature",glue="stepDefinitions")
+public class testRunner {
+	
+
+}
